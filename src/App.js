ac38a253e264c3b7facfc6890c7bc156abaafe06
Alex Krawiec <alex.krawiec@R7J1Y747QQ.local>
2024-10-01 09:39:56 -0700

    Add api call to hit all four product routes on backend

--- src/App.js
+++ src/App.js
@@ -2,20 +2,54 @@ import logo from './logo.svg';
 import alexPic from './img/alex.jpg'
 import './App.css';
 
+import { useState } from 'react';
+
+
 function App() {
+
+  const [data, setData] = useState(null);
+
+  
+  // function getKittenMittens() {
+  //   fetch('http://localhost:3001/products/kitten-mittens/')
+  //     .then(response => (console.log('response',response)))
+  //     .then(response => response.json())
+  //     .then(response => (console.log('jsonresponse', response)))
+  //     .then(json => setData(json))
+  //     .then(console.log('data', data))
+  //     .catch(error => console.error(error));
+  // }
+
+  async function getProduct(slug) {
+    const url = `http://localhost:3001/products/${slug}/`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log('json', json);
+      setData(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={alexPic} className="App-logo" alt="logo" />
         <div className='btn-container'>
-          <button className='btn'>This is a button</button>
-          <button className='btn'>This is a button</button>
-          <button className='btn'>This is a button</button>
-          <button className='btn'>This is a button</button>
+          <button className='btn' onClick={() => getProduct('kitten-mittens')}>Kitten Mittens</button>
+          <button className='btn' onClick={() => getProduct('doggles')}>Doggles</button>
+          <button className='btn' onClick={() => getProduct('clown-shoes')}>Clown Shoes</button>
+          <button className='btn' onClick={() => getProduct('nonfat-water')}>Nonfat Water</button>
         </div>
         <p>
           This is a React App
         </p>
+        <p>data</p>
         
       </header>
     </div>
@@ -23,3 +57,4 @@ function App() {
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://47f8fd7bfe831ff175cb6404677f0913@o4508010433216512.ingest.us.sentry.io/4508020663844864",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +32,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="fef256f4-083c-5509-b329-8477b3f916e5")}catch(e){}}();
+//# debugId=fef256f4-083c-5509-b329-8477b3f916e5
